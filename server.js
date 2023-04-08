a1f7feba4e0d80923990f020f3fd0af004f680e1
AlexCampean19 <alex.campean24@gmail.com>
2023-04-08 17:51:16 +0300

    schimbari

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 let express = require('express')
 let request = require('request');
-const sessionStorage = require('sessionstorage-for-nodejs');
+const bodyParser = require('body-parser')
 let store = require('store')
 let app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
-let city = 'Cluj-Napoca';
+app.use(bodyParser.urlencoded({ extended: true }))
 
-
-let url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}`
-app.get('/', function(req, res, next) {
+app.post('/', function(req, res, next) {
+    let city = req.body.city;
+    let url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}`
     request(url, function(error, response, body) {
         let date = JSON.parse(body)
+        console.log(body)
         let lat = date.results[0].latitude;
         let lon = date.results[0].longitude;
-        store.set('latitude', lat);
-        store.set('longitude', lon);
-        console.log(parseFloat(store.get('latitude').toFixed(2)))
-        next()
+        store.set('latitude', lat)
+        store.set('longitude', lon)
+        console.log(store.get('latitude'))
 
+        next()
     })
 })
-let api = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=52.52&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto";
-app.get('/', function(req, res, next) {
+app.post('/', function(req, res, next) {
+    let api = "https://api.open-meteo.com/v1/forecast?latitude=" + store.get("latitude") + "&longitude=" + store.get("longitude") + "&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto";
     request(api, function(error, response, body) {
         let vreme = JSON.parse(body)
         console.log(vreme)
             //console.log(vreme.current_weather.temperature)
+
         const weather = {
-            city: city,
+            city: req.body.city,
             temperature: vreme.current_weather.temperature,
+            description: vreme.current_weather.weathercode,
             temp_min: vreme.daily.temperature_2m_min[0],
             temp_max: vreme.daily.temperature_2m_max[0],
             ore: vreme.hourly.time,
@@ -37,16 +40,9 @@ app.get('/', function(req, res, next) {
             zile: vreme.daily.time,
             tempzilemax: vreme.daily.temperature_2m_max,
             tempzilemin: vreme.daily.temperature_2m_min,
-
         }
-        console.log(Math.round(vreme.daily.temperature_2m_max))
 
         res.render('weather', weather);
     })
 })
-
-
-
-
-
 app.listen(8001);
