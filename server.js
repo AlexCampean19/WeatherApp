7279ccb59a8deda21bd4fd54c4e7b0cdeae675c1
AlexCampean19 <alex.campean24@gmail.com>
2023-04-11 19:29:46 +0300

    schimbari

--- server.js
+++ server.js
@@ -13,17 +13,12 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', function(req, res, next) {
-    console.log(store.get('latitude'))
-
     let api = "https://api.open-meteo.com/v1/forecast?latitude=46.76667&longitude=23.6&current_weather=true&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto";
     request(api, function(error, response, body) {
         if (error) {
             res.render('weather', { weather: null, error: 'Error, please try again' });
         } else {
             let vreme = JSON.parse(body)
-            console.log(vreme.hourly.time)
-                //console.log(vreme.current_weather.temperature)
-
             res.render('weather', {
                 weather: vreme,
                 city: 'Cluj_Napoca',
@@ -38,41 +33,40 @@ app.get('/', function(req, res, next) {
                 descriptionday: vreme.daily.weathercode,
                 tempzilemax: vreme.daily.temperature_2m_max,
                 tempzilemin: vreme.daily.temperature_2m_min,
-                error: null
+                error: null,
+                suncloud: [1, 2],
+                fog: [45, 48],
+                drizzle: [51, 53, 55, 56, 57],
+                rain: [61, 63, 65, 66, 67, 80, 81, 82],
+                snow: [71, 73, 75, 77, 85, 86],
+                storm: [95, 96, 99],
             })
             next()
         }
-
-
     })
 })
 
 app.post('/', function(req, res, next) {
-
     let city = req.body.city;
-
     let url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}`
     request(url, function(error, response, body) {
         if (error) {
             res.render('weather', { weather: null, error: 'Error, please try again' });
         } else {
             let date = JSON.parse(body)
-
             if (date.results == undefined) {
                 res.render('weather', { weather: null, error: 'Error, please try again' });
             } else {
                 let lat = date.results[0].latitude;
                 let lon = date.results[0].longitude;
-
                 store.set('latitude', lat)
                 store.set('longitude', lon)
-
                 next()
             }
         }
-
     })
 })
+
 app.post('/', function(req, res, next) {
     let api = "https://api.open-meteo.com/v1/forecast?latitude=" + store.get("latitude") + "&longitude=" + store.get("longitude") + "&current_weather=true&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto";
     request(api, function(error, response, body) {
@@ -94,7 +88,13 @@ app.post('/', function(req, res, next) {
                 descriptionday: vreme.daily.weathercode,
                 tempzilemax: vreme.daily.temperature_2m_max,
                 tempzilemin: vreme.daily.temperature_2m_min,
-                error: null
+                error: null,
+                suncloud: [1, 2],
+                fog: [45, 48],
+                drizzle: [51, 53, 55, 56, 57],
+                rain: [61, 63, 65, 66, 67, 80, 81, 82],
+                snow: [71, 73, 75, 77, 85, 86],
+                storm: [95, 96, 99],
             })
         }
 
@@ -104,26 +104,18 @@ app.post('/', function(req, res, next) {
 
 let oras = [];
 app.post('/add_favorite', function(req, res) {
-
     oras.push(req.body)
-
     store.set('favoriteCity', oras)
 
 })
-
-
 
 app.get('/favorite', function(req, res) {
     let orasfav = store.get('favoriteCity')
     let orasadaugat = JSON.stringify(orasfav)
     let vremeoras = JSON.parse(orasadaugat)
-    console.log(vremeoras[0].oras)
     res.render('favorite', {
         vremeorase: vremeoras,
-
     })
 })
-
-
 
 app.listen(8001);
